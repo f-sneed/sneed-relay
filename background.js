3259da252ef4847b75a553957bfe9e7077959369
y a t s <140337963+y-a-t-s@users.noreply.github.com>
2023-10-21 23:02:29 -0400

    Add groundwork for settings page

---
+++ background.js
@@ -0,0 +1,13 @@
+// Define paths of extension pages.
+// The key is to be the basename of the file relative to the project root.
+const EXT_PAGES = {
+    config: "config.html"
+};
+// Referencing browser instead of chrome on chrome causes problems.
+if (typeof browser === "undefined")
+    var browser = chrome;
+
+// Open settings page on toolbar icon click.
+browser.browserAction.onClicked.addListener(() => {
+    let cfgTab = browser.tabs.create({url: EXT_PAGES.config});
+});
